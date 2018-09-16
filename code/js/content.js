6c782d491aa24314a316138f7eb780ee0b5e9a14
Rodrigo <Rodrigo@Macbook-de-Rodrigo.local>
2018-09-16 19:19:24 -0300

    first commit

---
+++ code/js/content.js
@@ -0,0 +1,9 @@
+;(function() {
+  console.log('[Cyclops] Running hide YouTube numbers extension')
+
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+})()
+
